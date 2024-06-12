185125518ca8e29fb65bd507088b7a2f80add436
Emre905 <emreyunust@hotmail.com>
2024-06-12 14:40:26 +0300

    optimize calculate_point and brute_force

--- brute_force.py
+++ brute_force.py
@@ -14,7 +14,11 @@ with open('cities.txt') as f:
 with open('roads.txt') as f:
     next(f) # skipping header
     EDGES = [tuple(i.strip().split(',')) for i in f] # stripping "\n" and making other , seperated terms a tuple
-    
+# get specific weight of each edges used to calculate single road points
+EDGES_DICT = {(city1,city2):int(weight) for city1,city2,weight,color in EDGES}
+# adding weight conversion for edge points. (3 weight road is 4 point etc)
+POINT_EDGE_DICT = {i:j for i,j in zip(range(1,7),[1,2,4,7,10,15])}
+
 # TICKETS are given by the game. The goal is to connect those 2 cities, 3rd input is the point you get from completing
 # (City A,City B,Points)
 with open('tickets.txt') as f:
@@ -25,17 +29,15 @@ with open('tickets.txt') as f:
 with open('city_locations.json') as f:
     CITY_LOCATIONS = json.load(f)
 
-
 # Build bidirectional weighted graph from the VERTICES and EDGES
-
 G = nx.MultiGraph() # use networkx library Multigraph
 G.add_nodes_from(VERTICES) #add vertices
 
 for edge in EDGES:
-    G.add_edge(edge[0], edge[1], weight = int(edge[2]), colour = edge[3]) #add edges with weight and colour
+    G.add_edge(edge[0], edge[1], 
+    weight = int(edge[2]), colour = edge[3]) #add edges with weight and colour
 
 # Steiner tree tries to find minimum sum of weights to connect given nods
-
 ''' Note: Finding optimal Steiner Tree is an NP problem,
 the function nx.steiner_tree() just approximates    the best solution. 
 Had "no attribute" problems for this function. It is copied from the documentation, 
@@ -43,14 +45,9 @@ made some slight changes and removed unnecessary functions:
 '''
 
 def steiner_tree(G, terminal_nodes, weight="weight"):
-    method = "mehlhorn"
-        
-    try:
-        algo = _mehlhorn_steiner_tree
-    except KeyError as e:
-        raise ValueError(f"{method} is not a valid choice for an algorithm.") from e
+    method = "mehlhorn"  
+    edges = _mehlhorn_steiner_tree(G, terminal_nodes, weight)
 
-    edges = algo(G, terminal_nodes, weight)
     # For multigraph we should add the minimal weight edge keys
     if G.is_multigraph():
         edges = (
@@ -102,55 +99,35 @@ def _remove_nonterminal_leaves(G, terminals):
         if n not in terminals_set and G.degree(n) == 1:
             G.remove_node(n)
 
-# This function connects given vertices with minimum weighted edges
-def connect_cities(list_cities):
-    edges_to_connect = _mehlhorn_steiner_tree(G, list_cities, weight = "weight")
-    G2 =steiner_tree(G, list_cities) # find the shortest path connecting all cities in list_cities
-    weight = int(G2.size(weight="weight")) # calculate it's weight
-    
-    return weight,G2
-
-# To calculate train cost and points of a collection of path, we can select some indexes
-# from 0 to 29 (len(TICKETS)=30). Then we're gonna calculate the weight, path and point.
-# (not including longest path point since it's single player)
-def calculate_point(indexes):
-    city_lst = [TICKETS[i][j] for i in indexes for j in range(2)] # add city1 and city2   
-    weight, G2 = connect_cities(city_lst)
-    point_ticket = 0
-    point_edge = 0
-    for i in indexes:
-        add = int(TICKETS[i][2]) # adding ticket points
-        point_ticket += add
-        
-    # get specific weight of each edges used to calculate single road points
-    EDGES_DICT = {(city1,city2):int(weight) for city1,city2,weight,color in EDGES}
-    
-    # adding weight conversion for edge points. (3 weight road is 4 point etc)
-    POINT_EDGE_DICT = {i:j for i,j in zip(range(1,7),[1,2,4,7,10,15])}
-    for i,j in G2.edges():
-        try: 
-            edge_weight = EDGES_DICT[(i,j)]
-        except KeyError:
-            edge_weight = EDGES_DICT[(j,i)]
-        finally:
-            point_edge += POINT_EDGE_DICT[edge_weight]
-    
-    point = point_ticket + point_edge # it's enough to return point, this way is just
-    # to be able to seperate ticket and edge points in the end
-            
-    return (point_ticket, point_edge), weight, G2
-
 # for brute force testing all possible routes
 def brute_force(n):
     max_score = 100
-    for ticket_amount in range(n,n+2): # should be (n,n+6) or (n,n+7) for n = 12
+    for ticket_amount in range(n,n+3): # should be (n,n+6) or (n,n+7) for n = 12
         idx_list = combinations(range(30),ticket_amount)
         for idx in idx_list: # idx_list is a generator. need to iterate it for each term
-            (p1,p2), w, G2 = calculate_point(list(idx)) # function takes list of indexes as input
-            if p1+p2 > max_score and w <= 45: # new record and weight(trains) <= 45
-                max_score = p1+p2
-                weight = w
-                best_idx = list(idx)
+            city_list = [TICKETS[i][j] for i in idx for j in range(2)] # add city1 and city2   
+            G2 =steiner_tree(G, city_list) # find the shortest graph connecting all cities in city_list
+            w = int(G2.size(weight="weight")) # calculate it's weight
+
+            if w <= 45: # calculate point if weight <= 45
+                point_ticket = 0
+                point_edge = 0
+                for i in idx:
+                    add = int(TICKETS[i][2]) # adding ticket points
+                    point_ticket += add
+                
+                for i,j in G2.edges(): # add edge(road) points
+                    try: 
+                        edge_weight = EDGES_DICT[(i,j)] # it may be reverse order
+                    except KeyError:
+                        edge_weight = EDGES_DICT[(j,i)]
+                    finally:
+                        point_edge += POINT_EDGE_DICT[edge_weight]
+
+                if point_ticket + point_edge > max_score:
+                    max_score = point_ticket + point_edge
+                    weight = w
+                    best_idx = list(idx)
         print(f'best {ticket_amount} ticket paths are achieved at {best_idx} giving {max_score} points with {weight} trains {time()-start:.2f} sec')
     return max_score, weight, best_idx
 start = time()
